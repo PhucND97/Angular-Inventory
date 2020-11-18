0397a95b21afe038564165183400169ba6cf1cbe
PhucND97 <phucdts97@gmail.com>
2020-11-18 13:05:18 +0700

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductRowComponent,
+    ProductImageComponent,
+    PriceDisplayComponent,
+    ProductDepartmentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
